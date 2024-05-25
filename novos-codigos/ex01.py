fa34109f48f404d1a700d5d95e7430f270c92b9e
RenatoRobert <164108253+RenatoRobert@users.noreply.github.com>
2024-05-25 18:22:35 -0300

    Update ex01.py

--- novos-codigos/ex01.py
+++ novos-codigos/ex01.py
@@ -1,1 +1,10 @@
 # Neste algoritmo, crie uma variável que armazene uma string e uma lista que armazena várias strings.
+
+minha_string = "Olá, mundo!"
+
+minha_lista_de_strings = ["Python", "JavaScript", "C++", "Java", "Ruby"]
+
+print("Conteúdo da variável string:", minha_string)
+
+print("Conteúdo da lista de strings:", minha_lista_de_strings)
+
